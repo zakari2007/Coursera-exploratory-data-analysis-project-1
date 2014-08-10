bbca2c52dc01dda158f22244c5769f2eaf818171
zoundi <zakari2007@users.noreply.github.com>
2014-08-10 23:05:59 +0900

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,1 +1,9 @@
- 
+ setwd("C://Users/zz/Documents/GSICS/econometrics/COURSERA/explor data analysis")# set working dir
+household_power_consumption <- read.csv("~/GSICS/econometrics/COURSERA/explor data analysis/household_power_consumption.txt", sep=";")#open in RView(household_power_consumption)#view
+mypowerConsumption <- powerConsumption[as.character(powerConsumption$Date) %in% c("1/2/2007", "2/2/2007"),] #set 2 days februrary
+mypowerConsumption$dateTime <- strptime(mypowerConsumption$dateTime, "%d/%m/%Y %H:%M:%S") #set time
+attach(mypowerConsumption)
+png("plot1.png", width=480, height=480, units="px")#characteristics png
+hist(as.numeric(as.character(Global_active_power)), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)") #plot1
+
+
